3371084a39b009cea9efcf2f1d9a2fc57b971644
Dimas Aditya Purwadi Putra <44697861+dimasAditya132@users.noreply.github.com>
2024-05-14 22:45:44 +0700

    delete project

--- F-BJV24001115-synergy7-dim-binfood-ch4/BinFood/src/main/java/com/example/BinFood/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BinFood.repository;
-
-import com.example.BinFood.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<Product, UUID>{
-}
