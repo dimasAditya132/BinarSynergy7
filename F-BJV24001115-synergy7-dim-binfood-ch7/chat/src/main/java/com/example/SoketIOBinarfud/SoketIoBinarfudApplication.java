fee6df9d520c3b5b27835154cf3d7ac3cd0edbaa
Dimas Aditya Purwadi Putra <dimasaaditya132@gmail.com>
2024-06-29 02:03:39 +0700

    socket io

---
+++ F-BJV24001115-synergy7-dim-binfood-ch7/chat/src/main/java/com/example/SoketIOBinarfud/SoketIoBinarfudApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoketIOBinarfud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoketIoBinarfudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoketIoBinarfudApplication.class, args);
+	}
+
+}
---
+++ F-BJV24001115-synergy7-dim-binfood-ch7/gateway/src/main/java/com/example/GatewayBinarfud/GatewayBinarfudApplication.java
@@ -0,0 +1,13 @@
+package com.example.GatewayBinarfud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBinarfudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBinarfudApplication.class, args);
+	}
+
+}
