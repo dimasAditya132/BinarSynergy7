e8f11f533e6cc5efb84f967d33ad7956bc977a0d
Dimas Aditya Purwadi Putra <dimasaaditya132@gmail.com>
2024-07-05 23:18:50 +0700

    socket io fixed

--- F-BJV24001115-synergy7-dim-binfood-ch7/chat/src/main/java/com/example/controller/SocketIOController.java
+++ F-BJV24001115-synergy7-dim-binfood-ch7/chat/src/main/java/com/example/controller/SocketIOController.java
@@ -42,8 +42,6 @@ public class SocketIOController {
                 message.setFrom(userId);
             }
 
-            System.out.println(message);
-
             socketIOServer.getBroadcastOperations().sendEvent(message.getTo(), message.getMessage());
             ackRequest.sendAckData("Message sent to " + message.getTo());
         }
